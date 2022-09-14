842d99f80e57cfcba15e97d34e5f9fa39078ea82
Aysuarex <aysuarex@gmail.com>
2022-09-14 14:45:41 +0100

    Updated files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,79 +1,84 @@
 #!/usr/bin/python3
-"""New Engine DB_Storage"""
-
+""" new class for sqlAlchemy """
 from os import getenv
-from sqlalchemy.orm import relationship
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import Base, BaseModel
-from models.amenity import Amenity
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
+from models.state import State
 from models.city import City
+from models.user import User
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
+from models.amenity import Amenity
 
 
 class DBStorage:
-    """New Engine DB_Storage"""
-
+    """ create tables in environmental"""
     __engine = None
     __session = None
-    classes = ["User", "State", "City", "Amenity", "Place", "Review"]
 
     def __init__(self):
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
 
-        db_parameters = {
-            "host": getenv("HBNB_MYSQL_HOST"),
-            "port": 3306,
-            "user": getenv("HBNB_MYSQL_USER"),
-            "pass": getenv("HBNB_MYSQL_PWD"),
-            "db": getenv("HBNB_MYSQL_DB")
-        }
-
-        self.__engine = create_engine('mysql+mysqldb://{user}:{pass}@{host}:\
-                                      {port}/{db}'.format(**db_parameters),
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
                                       pool_pre_ping=True)
 
-        if getenv("HBNB_ENV") == 'test':
-            Base.metada.drop_all(self.__engine)
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """return a dictionary with all objets of a class or all classes"""
-        mydict = {}
-        if cls is None:
-            for myclass in self.classes:
-                result = self.__session.query(myclass).all()
-                for obj in result:
-                    mydict[f"{obj.__class__}.{obj.id}"] = obj
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
         else:
-            result = self.__session.query(cls).all()
-            for obj in result:
-                mydict[f"{obj.__class__}.{obj.id}"] = obj
-        return mydict
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
 
     def new(self, obj):
-        """ add the object to the current database session """
-        if obj:
-            self.__session.add(obj)
+        """add a new element in the table
+        """
+        self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session """
+        """save changes
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """ delete from the current database session """
-        if obj is not None:
-            self.__session.delete(obj)
+        """delete an element in the table
+        """
+        if obj:
+            self.session.delete(obj)
 
     def reload(self):
-        """ create all tables in the database (feature of SQLAlchemy) """
+        """configuration
+        """
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(
-            bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
         self.__session = Session()
 
     def close(self):
-        """ call reload method"""
-        self.__session.remove()
+        """ calls remove()
+        """
+        self.__session.close()
--- models/place.py
+++ models/place.py
@@ -1,68 +1,83 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from curses import setupterm
-from os import getenv
-from re import A
-from sys import settrace
-from AirBnB_clone_v2.models import amenity
+"""This is the place class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
-place_amenity = Table('place_amenity', Base.metadata, Column('place_id',
-                String(60), ForeignKey('places.id'), nullable=False),
-                Column('amenity_id', String(60),
-                ForeignKey('amenities.id'), nullable=False))
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False))
+
 
 class Place(BaseModel, Base):
-    """ A place to stay """
+    """This is the class for Place
+    Attributes:
+        city_id: city id
+        user_id: user id
+        name: name input
+        description: string of description
+        number_rooms: number of room in int
+        number_bathrooms: number of bathrooms in int
+        max_guest: maximum guest in int
+        price_by_night:: pice for a staying in int
+        latitude: latitude in flaot
+        longitude: longitude in float
+        amenity_ids: list of Amenity ids
+    """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
-    
-
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", backref='place', cascade="all, delete")
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="place")
+
         amenities = relationship("Amenity", secondary=place_amenity,
-                            viewonly=False, cascade="all, delete",
-                            backref="places")
+                                 viewonly=False,
+                                 back_populates="place_amenities")
     else:
-        from models import storage
         @property
         def reviews(self):
-            """ Returns the list of Review instances with
-                place_id equals to the current Place.id
-            """
-            reviews = []
-            for place in storage.all(Review).values():
-                if place.id == self.place_id:
-                    reviews.append(place)
-            return reviews
+            """ Returns list of reviews.id """
+            var = models.storage.all()
+            lista = []
+            result = []
+            for key in var:
+                review = key.replace('.', ' ')
+                review = shlex.split(review)
+                if (review[0] == 'Review'):
+                    lista.append(var[key])
+            for elem in lista:
+                if (elem.place_id == self.id):
+                    result.append(elem)
+            return (result)
 
         @property
         def amenities(self):
-            """ Function that returns the list of City
-            instances with state_id """
+            """ Returns list of amenity ids """
+            return self.amenity_ids
 
-            amenities_list = []
-            for amenity in storage.all(Amenity).values():
-                if amenity.id == self.amenity_id:
-                    amenities_list.append(amenity)
-            return amenities_list
- 
-
-        @amenities.setter 
-        def ameninities(self, arg):
-            """ Function setter amenities """
-            if arg.__class__.name__ == "Amenity":
-                self.amenity_ids.append(arg)
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ Appends amenity ids to the attribute """
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)

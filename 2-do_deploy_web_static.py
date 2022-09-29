812fc48c05abac469a336654cb458da43d7e5ec8
kabeer Akanni <77249754+kabeer-prog@users.noreply.github.com>
2022-09-29 20:37:11 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['35.175.119.246', '44.192.54.126']
 
 
 def do_deploy(archive_path):

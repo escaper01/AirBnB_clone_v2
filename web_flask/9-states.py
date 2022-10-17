f1a5e72022f8b47d2f6206871d9e528792ad4228
kabeer Akanni <77249754+kabeer-prog@users.noreply.github.com>
2022-10-17 06:27:08 +0200

    files

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,48 +1,38 @@
 #!/usr/bin/python3
+"""
+flask model
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
-
-
 app = Flask(__name__)
+storage.all()
 
 
 @app.teardown_appcontext
-def tear_down(self):
-    """ Tear down app context """
+def teardown_data(self):
+    """
+        refrech data
+    """
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
-def list_all_states():
-    """ Lists states from database
-    Returns:
-        HTML
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id=None):
     """
-    dict_states = storage.all(State)
-    all_states = []
-    for k, v in dict_states.items():
-        all_states.append(v)
-    return render_template('9-states.html', all_states=all_states)
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def find_state(id):
-    """ Lists states from database with specific id
-    Args:
-        id (str): id
-    Returns:
-        HTML
+        list state by id if found
     """
-    dict_states = storage.all(State)
-    all_states = []
-    all_states_id = []
-    for k, v in dict_states.items():
-        all_states_id.append(v.id)
-        all_states.append(v)
-    return render_template('9-states.html', all_states=all_states,
-                           all_states_id=all_states_id, id=id)
+    info = []
+    states = storage.all(State)
+    if id is None:
+        for k in states:
+            info.append(states[k])
+    else:
+        id = 'State.' + id
+        info = states.get(id)
+    return render_template('9-states.html', states=info, id=id)
+    return render_template('9-states.html', state=id)
 
 
 if __name__ == "__main__":
